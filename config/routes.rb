7cc063ddd17b7f16486564939f1a353e25c6610c
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2021-01-28 06:12:35 +0900

    addressフォーム編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :customers, controllers: {
     registrations: 'user/registrations'
   }
+  devise_scope :customer do
+    get 'addresses', to: 'customers/registrations#new_address'
+    post 'addressesa', to: 'customers/registrations#create_address'
+  end
   root to: "items#index"
 end
