68fddcd1ce314f9522cb147bdd5829e791cfce67
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2021-01-29 06:51:57 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,23 @@
 class ItemsController < ApplicationController
+  
+  def index
+    @itme = Item.order(created_at: :desc).limit(5)
+  end
+
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
 end
