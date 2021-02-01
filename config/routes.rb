b1ae49557ffee787bc1654e61fad2286999ffdfd
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2021-02-01 14:02:32 +0900

    カード登録機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,24 @@
 Rails.application.routes.draw do
   get 'cards/new'
-  devise_for :producers, controllers: {
-    registrations: 'producers/registrations'
-  }
-  devise_scope :producer do
-    get 'addresses', to: 'producers/registrations#new_address'
-    post 'addresses', to: 'producers/registrations#create_address'
-  end
-
+  
   devise_for :customers, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
     registrations: 'customers/registrations'
   }
   devise_scope :customer do
     get 'addresses', to: 'customers/registrations#new_address'
     post 'addresses', to: 'customers/registrations#create_address'
+  end
+
+  devise_for :producers, controllers: {
+    sessions:      'producers/sessions',
+    passwords:     'producers/passwords',
+    registrations: 'producers/registrations'
+  }
+  devise_scope :producer do
+    get 'addresses', to: 'producers/registrations#new_address'
+    post 'addresses', to: 'producers/registrations#create_address'
   end
 
   resources :items
